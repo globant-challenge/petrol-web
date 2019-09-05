1e21f2a1f0bae735bb936fa95d678641e25a1bdf
denis-conte <denis.conte@globant.com>
2019-09-05 01:17:04 -0300

    rename-icon

--- src/WeatherWidget/index.js
+++ src/WeatherWidget/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import nublado from '../Assets/Icons/nublado.png';
+import cloudy from '../Assets/Icons/cloudy.png';
 import './styles.scss';
 
 function WeatherWidget() {
   return (
     <div className="weather-widget">
       <div className="weather-widget__container">
-        <img src={nublado} className="weather-widget__icon" alt="weather" />
+        <img src={cloudy} className="weather-widget__icon" alt="weather" />
         <h4 className="weather-widget__text">22º C - 45F</h4>
       </div>
     </div>
